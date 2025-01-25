4a823475748700e0396174e780c379a45714cc97
akash <a@b.com>
2025-01-25 14:49:27 +0530

    origin

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 @app.route('Python App Hosting')
 def hello_geek():
     return '<h1>Hello from Flask & Docker</h1>'
-@app.route('/hi')
-def hell():
-    return '<h1>Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii from Flask & Docker</h1>'
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
